dffc0a8961bb0d88e3bbd7b5233a92c7728c1400
Michael Panter <makinglife1231@gmail.com>
2023-02-26 00:01:52 -0500

    added row numbering

--- addBooks.js
+++ addBooks.js
@@ -6,14 +6,17 @@ fetch('books-2021.json')
   let placeholder = document.querySelector('#book-list');
 
   let output = '';
+  let i = 1;
   for(let book of books){
     output += `
       <tr>
+        <td>${i}</td>
         <td>${book.author}</td>
         <td>${book.title}</td>
         <td>${book.date}</td>
       </tr>
     `;
+    i++;
   }
 
   placeholder.innerHTML = output;
